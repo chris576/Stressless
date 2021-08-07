5248ca62830ce9cd21f8ed20ba4b9aa1320a3eb0
chhu <christopher.huebner@gmx.net>
2021-08-07 14:25:34 +0200

    First commit.

---
+++ app/src/main/java/com/example/stressless/model/Community.kt
@@ -0,0 +1,8 @@
+package com.example.stressless.model
+
+data class Community(
+    val name: String,
+    val id: String,
+    val picture_link: String,
+    val description: String
+);
---
+++ app/src/main/java/com/example/stressless/model/User.kt
@@ -0,0 +1,9 @@
+package com.example.stressless.model
+
+data class User(
+    val name: String,
+    val id: String,
+    val picture: String,
+    val mail: String?,
+    val phone: Int?
+);
